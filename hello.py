751d2eb82ba0b669078b92cfc5e6647f56dcc0ae
AbstractMonkey <sean.hobin@gmail.com>
2019-12-10 06:26:55 -0500

    add files

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return 'Index Page'
+
+    @app.route('/hello')
+    def hello():
+        return render_template('base.html', title='hello')
+
+    return app
