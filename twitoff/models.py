10c7fbf48fe3393ee8b49b40909b068e431d2cb1
AbstractMonkey <sean.hobin@gmail.com>
2019-12-10 22:39:20 -0500

    file folder

---
+++ twitoff/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+class User(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(20), nullable=False)
+
+class Tweet(DM.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    text = DB.Column(DB.Unicode(280), nullable=False)
+
